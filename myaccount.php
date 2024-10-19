a847ae2c4c9fb2c0fb2621ea56a7e055959cb4a9
Naman Anand <naman.na@outlook.com>
2024-10-19 20:54:03 +0530

    Update myaccount.php

--- myaccount.php
+++ myaccount.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title>PHPGURUKUL Job Portal</title>
+<title> Job Portal</title>
 <link rel="stylesheet" href="css/style.css" type="text/css" />
 <script type="text/javascript" src="js/jquery-1.4.2.min.js"></script>
 <script type="text/javascript" src="js/script.js"></script>
