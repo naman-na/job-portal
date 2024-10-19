3ea9db653b89797e48b919e462545aa02b76db2b
Naman Anand <naman.na@outlook.com>
2024-10-19 20:54:28 +0530

    Update EmployeeVerification.php

--- EmployeeVerification.php
+++ EmployeeVerification.php
@@ -3,7 +3,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title>PHPGURUKUL Job Portal/EmployeeVarification</title>
+<title> Job Portal/EmployeeVarification</title>
 <link rel="stylesheet" href="css/style.css" type="text/css" />
 </head>
 
